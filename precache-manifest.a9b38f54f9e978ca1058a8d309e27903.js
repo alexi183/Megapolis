45d0b6e89b53e7a983f7b24fbe01f710ea7eef79
Alex <format255@bk.ru>
2019-08-11 23:00:31 +0300

    Updates

---
+++ precache-manifest.a9b38f54f9e978ca1058a8d309e27903.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a447f20961e2b5068a94b86789ad575d",
+    "url": "/Megapolis/index.html"
+  },
+  {
+    "revision": "d63e6c084857d978f199",
+    "url": "/Megapolis/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "372cf57f0b5f65f122c4",
+    "url": "/Megapolis/static/css/main.4496fff8.chunk.css"
+  },
+  {
+    "revision": "d63e6c084857d978f199",
+    "url": "/Megapolis/static/js/2.226fca48.chunk.js"
+  },
+  {
+    "revision": "372cf57f0b5f65f122c4",
+    "url": "/Megapolis/static/js/main.5b2adeaa.chunk.js"
+  },
+  {
+    "revision": "ee6b188d02a34e3e64e1",
+    "url": "/Megapolis/static/js/runtime~main.8dbb114f.js"
+  },
+  {
+    "revision": "520fee8398160e9fed1295eab025c603",
+    "url": "/Megapolis/static/media/close.520fee83.svg"
+  },
+  {
+    "revision": "6d5f731f698461dfa1e31ec414c97c31",
+    "url": "/Megapolis/static/media/delete.6d5f731f.svg"
+  },
+  {
+    "revision": "0d59e66e0e4fcb6b6383e62317f8ab60",
+    "url": "/Megapolis/static/media/edit.0d59e66e.svg"
+  }
+]);
